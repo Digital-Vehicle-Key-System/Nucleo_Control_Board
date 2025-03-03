a6cd08dbe1b6d272a633f31a55293742cc0493bf
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-03-03 15:59:27 +0200

    configure external interrupts at PC7 to be ready for receive updated users profile

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -69,6 +69,12 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 		/* Send GPS Reading to Connectivity */
 		//UART Code
 	}
+
+	if(GPIO_Pin == GPIO_PIN_7)
+	{
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		/* Call UART DMA to be ready to receive new data */
+	}
 }
 
 /* USER CODE END 0 */
@@ -240,6 +246,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA8 */
   GPIO_InitStruct.Pin = GPIO_PIN_8;
